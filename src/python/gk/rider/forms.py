ef6e0259ae9b3ac61a76fd3d712d2c7f0b6afa01
Chris Targett <chris@xlevus.net>
2022-05-24 21:56:11 +1200

    Removing django-registration

--- src/python/gk/rider/forms.py
+++
@@ -1,8 +0,0 @@
-from django_registration.forms import RegistrationForm as BaseRegistrationForm
-
-from . import models
-
-
-class RegistrationForm(BaseRegistrationForm):
-    class Meta(BaseRegistrationForm.Meta):
-        model = models.Rider
