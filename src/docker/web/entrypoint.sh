1147bd12f0ee319ff597f2607482abee6c45d9e2
Chris Targett <chris@xlevus.net>
2024-03-31 16:12:10 +1300

    WIP: Secretifying secret key

--- src/docker/web/entrypoint.sh
+++ src/docker/web/entrypoint.sh
@@ -1,6 +1,14 @@
 #!/bin/bash
 set -e
 
+if [ -z "$SECRET_KEY_FILE" ]; then
+    export SECRET_KEY=$(cat "$SECRET_KEY_FILE")
+fi
+
+if [ -z "$DATABASE_URL_FILE" ]; then
+    export DATABASE_URL=$(cat "$DATABASE_URL_FILE")
+fi
+
 if [ -z "$DATABASE_URL" ]; then
     echo "DATABASE_URL is unset"
     exit 1
