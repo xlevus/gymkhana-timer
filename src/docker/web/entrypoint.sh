2bb5d7a23a13964016cafb1b9066ee23e49eee6f
Chris Targett <chris@xlevus.net>
2024-03-31 19:12:50 +1300

    Moving to VPS hosting (#7)
    Moving from fly.dev to generic VPS.

--- src/docker/web/entrypoint.sh
+++ src/docker/web/entrypoint.sh
@@ -1,9 +1,22 @@
 #!/bin/bash
 set -e
 
+if [ -f "$SECRET_KEY_FILE" ]; then
+    export SECRET_KEY=$(cat "$SECRET_KEY_FILE")
+fi
+
+if [ -f "$DATABASE_URL_FILE" ]; then
+    export DATABASE_URL=$(cat "$DATABASE_URL_FILE")
+fi
+
 if [ -z "$DATABASE_URL" ]; then
     echo "DATABASE_URL is unset"
     exit 1
+fi
+
+if [ -d "$STATIC_ROOT" ]; then
+    echo "Collecting Static"
+    /manage.pex collectstatic --noinput
 fi
 
 echo "Migrating"
