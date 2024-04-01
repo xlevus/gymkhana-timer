226e1d0f4ad1badb61a4a3f946c9f95e9f784296
Chris Targett <chris@xlevus.net>
2024-04-01 14:59:05 +1300

    Basic course images (#8)
    * Basic course images
    
    * Configuration for media

--- src/python/gk/django/service.py
+++ src/python/gk/django/service.py
@@ -31,9 +31,9 @@ class Service:
         args = sys.argv
         if len(sys.argv) == 2 and sys.argv[1] == "runserver":
             # We rely on Pants's reloading, so turn off Django's (which doesn't interact
-            # well with Pex: Pex's re-exec logic causes Django's re-exec to misdetect 
+            # well with Pex: Pex's re-exec logic causes Django's re-exec to misdetect
             # its entry point, see https://code.djangoproject.com/ticket/32314).
-            # TODO: Some way to detect that we're in a `./pants run`, and only set 
+            # TODO: Some way to detect that we're in a `./pants run`, and only set
             # --noreload
             #  in that case, so that users can run manage.py directly if they want to.
             args += ["--noreload"]
