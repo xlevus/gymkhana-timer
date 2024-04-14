bcbf2d029c0fffffa1686f60933cc598739e9332
Chris Targett <chris@xlevus.net>
2024-04-15 07:53:30 +1200

    WIP: Refactored back to laser sensor

--- src/micropython/timer.py
+++ src/micropython/timer.py
@@ -27,54 +27,46 @@ class Init(State):
 
 class Calibrate(State):
     async def enter(self):
-        distance = self.machine.distance
-
-        range = distance.read()
         counts = 10
         await uasyncio.sleep_ms(100)
 
         pins = (RED_LED, GREEN_LED)
+        
         while counts > 0:
             pins[0].off()
             pins[1].on()
             pins = (pins[1], pins[0])
             await uasyncio.sleep(0.5)
 
-            curr_range = distance.read()
-            diff = range - curr_range
-            if abs(diff) <= 10:
-                logging.log(logging.INFO, f"Range OK: {curr_range} [{diff}]")
+            if LASER_PIN.value():
                 counts -= 1
             else:
-                logging.log(logging.INFO, f"Range ERR: {curr_range} [{diff}]")
+                logging.log(logging.WARN, "Calibration Reset")
                 counts = 10
-                range = curr_range
-
-        self.target_range = range
 
     async def tick(self):
-        return Ready(self.target_range)
+        return Ready()
 
 
 class Ready(State):
-    def __init__(self, target_range):
-        logging.log(logging.INFO, f"Target range: {target_range}")
-        self.target_range = target_range
+    def __init__(self):
+        self.trigger = uasyncio.ThreadSafeFlag()
+        self.start_time = None
 
     async def enter(self):
         RED_LED.off()
         GREEN_LED.on()
 
     async def tick(self):
-        await uasyncio.sleep_ms(1)
-        range = self.machine.distance.read()
-        if (range + 10) < self.target_range:
-            return Lap(self.target_range, time.gmtime())
+        if LASER_PIN.value() == 0:
+            start_time = time.ticks_ms()
+            await uasyncio.sleep_ms(2)
+            if LASER_PIN.value() == 0:
+                return Lap(start_time)
 
 
 class Lap(State):
-    def __init__(self, target_range, start_time):
-        self.target_range = target_range
+    def __init__(self, start_time):
         self.start_time = start_time
 
     async def enter(self):
@@ -85,15 +77,19 @@ class Lap(State):
         GREEN_LED.off()
 
     async def tick(self):
-        await uasyncio.sleep_ms(1)
-        range = self.machine.distance.read()
-        if (range + 10) < self.target_range:
-            return EndLap(self.start_time, time.gmtime())
+        if LASER_PIN.value() == 0:
+            end_time = time.ticks_ms()
+            await uasyncio.sleep_ms(2)
+            if LASER_PIN.value() == 0:
+                return EndLap(self.start_time, end_time)
+
+
 
 
 class EndLap(State):
     def __init__(self, start_time, end_time):
-        logging.log(logging.INFO, f"LAP: {start_time} {end_time}")
+        duration = time.ticks_diff(end_time, start_time)
+        logging.log(logging.INFO, f"LAP: {duration} (S:{start_time} E:{end_time})")
 
     async def tick(self):
         return Calibrate()
@@ -103,12 +99,12 @@ async def main():
     tasks = []
 
     try:
-        i2c = machine.I2C(0, scl=Pin(9), sda=Pin(8))
-        distance = VL53L1X(i2c)
+        #i2c = machine.I2C(0, scl=Pin(9), sda=Pin(8))
+        #distance = VL53L1X(i2c)
 
         sm = StateMachine(
-            i2c=i2c,
-            distance=distance,
+            #i2c=i2c,
+            #distance=distance,
         )
 
         await sm.change(Init())
