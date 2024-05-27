fa04904f75db80de8d93c00e95691114c331e77f
Chris Targett <chris@xlevus.net>
2024-05-27 13:32:49 +1200

    Working tfluna driver

--- src/micropython/timer/tfluna.py
+++ src/micropython/timer/tfluna.py
@@ -1,4 +1,8 @@
+import logging
+import uasyncio
 import struct
+
+logger = logging.getLogger(__name__)
 
 
 class TfLuna:
@@ -12,56 +16,89 @@ class TfLuna:
     _ID_SAVE_SETTINGS = 0x11
     _ID_ON_OFF_MODE = 0x3B
 
-    _OUTPUT_DISTANCE = "dist"
-    _OUTPUT_DATA = "data"
-    _OUTPUT_UNKNOWN = "unkn"
-
     def __init__(self, uart):
         self._uart = uart
-    def _read(self):
-        pass
-    def _write(self, command: int, format: str, *values):
-        msg = struct.pack(format+'x', *values)
-        header = struct.pack("<bbb", self._CMD_HEADER, len(msg) + 3, command)
-        self._uart.write(header+msg)
-    def _flush_buffer(self):
-        while self._uart.read():
-            pass
-    def read(self):
-        header = self._uart.read(2)
-        if header is None:
-            return None, None
-        elif header == b'YY':
-            distance, amplitude, temp = struct.unpack('<HHHx', self._uart.read(7))
-            return self._OUTPUT_DISTANCE, {
-                "distance_cm": distance,
-                "amplitude": amplitude,
-                "temp": temp/8 - 256,
-            }
-        elif header[0] == self._CMD_HEADER:
-            data = self._uart.read(header[1] - 2)
-            return self._OUTPUT_DATA, header+data
-        else:
-            return self._OUTPUT_UNKNOWN, header
-    def output_enable(self, enable: bool = True):
-        self._write(
+        self._sreader = uasyncio.StreamReader(self._uart)
+
+        self.distance = None
+        self.amplitude = None
+        self.temp = None
+
+        uasyncio.create_task(self._run())
+        self._die = uasyncio.Event()
+
+        self._cmdlock = uasyncio.Lock() 
+        self._cmdset = uasyncio.Event()
+        self._cmdresult = None
+
+    async def _write(self, command: int, format: str, *values, resp_format=None):
+        await self._cmdlock.acquire()
+        try:
+            self._cmdset.clear()
+            msg = struct.pack(format+'x', *values)
+            header = struct.pack("<bbb", self._CMD_HEADER, len(msg) + 3, command)
+            await self._sreader.awrite(header+msg)
+            if resp_format:
+                logger.debug("Awaiting Response")
+                await self._cmdset.wait()
+                resp_command, *resp_data = self._cmdresult
+                if resp_command != command:
+                    logger.debug(f"Mismatched reply, {resp_command} expected {command}")
+                    return None
+                return struct.unpack(resp_format, self._cmdresult)
+        finally:
+            self._cmdlock.release()
+            self._cmdresult = None
+
+    async def _run(self):
+        while not self._die.is_set():
+            header = await self._sreader.read(2)
+
+            if header is None:
+                await uasyncio.sleep(0)
+                continue
+
+            elif header == b'YY':
+                try:
+                    self.distance, self.amplitude, temp = struct.unpack("<HHHx", await self._sreader.read(7))
+                    self.temp = temp/8 - 256
+                except ValueError:
+                    pass
+
+            elif header[0] == self._CMD_HEADER:
+                data = await self._sreader.read(header[1] - 2)
+                self._cmdresult = data
+                self._cmdset.set()
+
+            else:
+                logger.debug(f"Wat? {header!r}")
+
+        logger.debug("Run DIE")
+    
+    async def stop(self):
+        self._die.set()
+
+    async def output_enable(self, enable: bool = True):
+        await self._write(
             self._ID_OUTPUT_ENABLE, '<b', enable
         )
-        if not enable:
-            self._flush_buffer()
-    def soft_reset(self):
-        self._write(self._ID_SOFT_RESET, "")
-    def set_output_frequency(self, frequency_hz: int):
-        self._write(self._ID_SAMPLE_FREQ, "<H", frequency_hz)
-        self._flush_buffer()
-    def restore_defaults(self):
-        self._write(self._ID_RESTORE_DEFAULT, "")
-    def save_settings(self):
-        self._write(self._ID_SAVE_SETTINGS, "")
+
+    async def soft_reset(self):
+        await self._write(self._ID_SOFT_RESET, "", wait_for_resp=False)
+
+    async def set_output_frequency(self, frequency_hz: int):
+        await self._write(self._ID_SAMPLE_FREQ, "<H", frequency_hz, resp_format="<bx")
+
+    async def restore_defaults(self):
+        await self._write(self._ID_RESTORE_DEFAULT, "")
+
+    async def save_settings(self):
+        await self._write(self._ID_SAVE_SETTINGS, "")
+
     IRQ_MODE_DISABLE = 0x00
     IRQ_MODE_HIGH = 0x01
     IRQ_MODE_LOW = 0x02
-    def irq_mode(self, mode: int, distance_cm: int, zone_cm: int, delay1_ms: int, delay2_ms: int):
+    async def irq_mode(self, mode: int, distance_cm: int, zone_cm: int, delay1_ms: int, delay2_ms: int):
         """Place TfLuna into "on/off" mode.
         This will set Pin6 high if something enters betwen it and `distance_cm` for `delay1_ms`, 
         and return it low if the object has moved `zone_cm` for a duration of `delay2_ms`.
@@ -71,26 +108,50 @@ class TfLuna:
         NOTE: The IRQ pin will only ever trigger every one `output_frequency` cycle. Disabling
         the output or setting the output_frequency to 0 will disable the IRQ pin.
         """
-        self._write(
+        await self._write(
             self._ID_ON_OFF_MODE, "<BHHHH", 
             mode,
             distance_cm,
             zone_cm,
             delay1_ms,
-            delay2_ms
+            delay2_ms,
+            resp_format="<bx"
         )
-    def trigger(self):
-        self._write(self._ID_SAMPLE_TRIG, "")
-    def distance(self):
-        """Attempt to get the current distance."""
-        self._flush_buffer()
-        while True:
-            type, value = self.read()
-            if type == self._OUTPUT_DISTANCE:
-                return value
+
+    async def trigger(self):
+        await self._write(self._ID_SAMPLE_TRIG, "", wait_for_resp=False)
+
+    async def get_version(self):
+        return await self._write(self._ID_GET_VERSION, "", resp_format="<bbbx")
 
 
-tf = TfLuna(uart)
-tf.soft_reset()
-tf.output_enable(True)
-tf.set_frequency(0)
+# if __name__ == '__main__':
+#     tf = TfLuna(uart)
+#     tf.soft_reset()
+#     tf.output_enable(True)
+#     tf.set_frequency(0)
+
+async def _test(uart):
+    tf = TfLuna(uart)
+
+    await uasyncio.sleep(1)
+
+    print("Version: ", await tf.get_version())
+    await tf.set_output_frequency(100)
+
+    await tf.irq_mode(tf.IRQ_MODE_HIGH, 60, 10, 10, 10)
+    import machine
+    x = machine.Pin(10, machine.Pin.IN)
+
+    while True:
+        print(f"Distance: {tf.distance}  X:{x.value()}")
+        await uasyncio.sleep(1)
+
+
+def test(uart):
+    import logging
+    logging.basicConfig(level=logging.DEBUG)
+    import machine
+    uart = uart or machine.UART(1, tx=8, rx=9, baudrate=115200)
+    uasyncio.run(_test(uart))
+    return uart
